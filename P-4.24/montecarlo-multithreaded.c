610ebd4932cdc32f0ff9b593ef068031df94f129
achintya-k <67586625+achintya-k@users.noreply.github.com>
2021-11-27 23:11:30 +0530

    Add files via upload

---
+++ P-4.24/montecarlo-multithreaded.c
@@ -0,0 +1,73 @@
+/* multithreaded version of the Monte Carlo method to find the value of Pi 
+Achintya K N - 191ec201
+*/
+
+#include <stdio.h>
+#include <pthread.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+int i = 0;	
+int number_of_points = 0;
+int circle_count  = 0;
+
+unsigned int seed = 2;
+
+// Random number generation using Linear Congruency when threads are involved
+double Randint (long i)
+{
+  seed = seed * 1103515245 + 123456;
+
+  return seed / (double)UINT_MAX;
+}
+
+// counts the number of randomly initialized points that fell inside the unit circle inscribed in the 1*1 square
+void *count(void *X)
+{ 
+    /* initialize random numbers */
+    for (i=0; i < number_of_points; i++)
+    {	
+        double X = (double) Randint(i); // randomly choosing values for X and Y
+        double Y = (double) Randint(i);
+        
+        if (((X * X) + (Y * Y)) <= 1)
+        {
+            circle_count++;
+        }
+    }
+    pthread_exit(0);
+}
+
+void exec()
+{
+    int count_norm = 0;
+
+}
+
+int main()
+{
+	
+    clock_t multi;
+    multi = clock ();
+    pthread_t thread;
+
+    // do-while loop for user prompts
+    do
+	{
+        printf("Enter a whole number for the number of points you would like to use to calculate the value of Pi using the Monte Carlo method: \n");
+        scanf("%d", &number_of_points);  	
+	}while (number_of_points < 1);
+  
+    pthread_create(&thread, NULL, &count, NULL); // creating a pthread which parallelly computes the number of points that fall in the unit circle
+    pthread_join(thread, NULL); // this waits for the termination of the above thread
+    
+    double pi = 4.0 * circle_count/number_of_points;
+    
+    printf("The approximate value of Pi for the desired amount of points (%d) is: %f (multithreaded version) \n", number_of_points, pi); 
+    multi = clock () - multi;
+    double time_spent = ((double)multi) / CLOCKS_PER_SEC;
+    printf("Time taken for execution: %f \n", time_spent);
+    return 0;
+}
